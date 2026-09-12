f53265563ed30dab924bc66a2ed237bbb0f2c0fc
saishetty1623-art <saishetty1623@gmail.com>
2026-09-12 12:18:43 +0530

    Delete SUPABASE_REALTIME.sql

--- SUPABASE_REALTIME.sql
+++
@@ -1,3 +0,0 @@
--- Run this once in Supabase SQL Editor.
--- It allows tracker_state changes to be delivered through Supabase Realtime.
-alter publication supabase_realtime add table public.tracker_state;
